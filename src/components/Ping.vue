340ccc46f0522581c66b5e7bf5d23e7687c5b4a0
Marcel Wlotzka <marcel@wlotzka.org>
2022-11-21 09:53:57 +0100

    Added annotations

--- src/components/Ping.vue
+++ src/components/Ping.vue
@@ -1,69 +1,8 @@
 <script setup>
-import { ref, watch, computed } from 'vue';
-import { useQuasar } from 'quasar';
 import { useAppStore } from '@/stores/app.js';
-import de from 'apexcharts/dist/locales/de.json';
-import en from 'apexcharts/dist/locales/en.json';
-const $q = useQuasar();
+import Statistics from '@/components/Statistics.vue';
 
 const store = useAppStore();
-const chart = ref([]);
-
-const chartOptions = computed(() => ({
-  chart: {
-    locales: [de, en],
-    defaultLocale: $q.lang.isoName === 'de' ? 'de' : 'en',
-    toolbar: {
-      show: false
-    }
-  },
-  dataLabels: {
-    enabled: false
-  },
-  markers: {
-    size: 0,
-    style: 'hollow'
-  },
-  xaxis: {
-    type: 'datetime',
-    tickAmount: 6
-  },
-  yaxis: {
-    labels: {
-      formatter(value) {
-        return `${Math.round(value)} ms`;
-      }
-    }
-  },
-  stroke: {
-    curve: 'straight',
-    width: 1
-  },
-  tooltip: {
-    x: {
-      format: 'dd MMM yyyy'
-    }
-  },
-  legend: {
-    showForSingleSeries: true
-  },
-  fill: {
-    type: 'gradient',
-    gradient: {
-      shadeIntensity: 1,
-      opacityFrom: 0.7,
-      opacityTo: 0.9,
-      stops: [0, 100]
-    }
-  }
-}));
-
-watch(() => $q.lang.isoName, () => {
-  chart.value.forEach(async(c) => {
-    await c.updateOptions(chartOptions.value);
-    c.refresh();
-  });
-});
 </script>
 
 <template>
@@ -71,14 +10,7 @@ watch(() => $q.lang.isoName, () => {
     <div v-for="(location, i) in store.locations" :key="i" class="row">
       <div class="col">
         <q-card class="q-mb-md">
-          <apexchart
-            ref="chart"
-            width="100%"
-            height="200"
-            type="area"
-            :options="chartOptions"
-            :series="[{ data: location.history, name: location.address }]"
-          />
+          <Statistics :location="location" />
         </q-card>
       </div>
     </div>
---
+++ src/components/Statistics.vue
@@ -0,0 +1,125 @@
+<script setup>
+import { ref, watch, computed } from 'vue';
+import { useQuasar } from 'quasar';
+import de from 'apexcharts/dist/locales/de.json';
+import en from 'apexcharts/dist/locales/en.json';
+const $q = useQuasar();
+
+const chart = ref(null);
+const props = defineProps({
+  location: {
+    type: Object,
+    required: true
+  }
+});
+const history = computed(() => props.location.history.map(e => typeof e === 'number' ? [e, null] : e));
+const annotations = computed(() => {
+  let res = props.location.history.reduce((acc, e) => {
+    if (typeof e === 'number' && acc.last !== null) {
+      acc.areas.push({
+        x: acc.last[0],
+        x2: e,
+        fillColor: '#90a4ae',
+        opacity: 0.2
+      });
+    } else if (typeof e !== 'number' && e[1] === null && acc.start === null && acc.last !== null) {
+      acc.start = acc.last[0];
+    } else if (typeof e !== 'number' && e[1] === null && acc.start === null && acc.last === null) {
+      acc.start = e[0];
+    } else if (typeof e !== 'number' && e[1] !== null && acc.start !== null) {
+      acc.areas.push({
+        x: acc.start,
+        x2: e[0],
+        fillColor: '#f44336',
+        opacity: 1
+      });
+
+      acc.start = null;
+    }
+
+    acc.last = e;
+
+    return acc;
+  }, { last: null, start: null, areas: [] });
+
+  return res.areas;
+});
+
+const chartOptions = computed(() => ({
+  chart: {
+    locales: [de, en],
+    defaultLocale: $q.lang.isoName === 'de' ? 'de' : 'en',
+    toolbar: {
+      show: false
+    },
+    zoom: {
+      enabled: false
+    }
+  },
+  annotations: {
+    xaxis: annotations.value
+  },
+  dataLabels: {
+    enabled: false
+  },
+  markers: {
+    size: 0,
+    style: 'hollow'
+  },
+  xaxis: {
+    type: 'datetime',
+    tickAmount: 6
+  },
+  yaxis: {
+    labels: {
+      formatter(value) {
+        return `${Math.round(value)} ms`;
+      }
+    }
+  },
+  stroke: {
+    curve: 'straight',
+    width: 1
+  },
+  tooltip: {
+    x: {
+      format: 'dd MMM yyyy'
+    }
+  },
+  legend: {
+    showForSingleSeries: true
+  },
+  fill: {
+    type: 'gradient',
+    gradient: {
+      shadeIntensity: 1,
+      opacityFrom: 0.7,
+      opacityTo: 0.9,
+      stops: [0, 100]
+    }
+  }
+}));
+
+console.log(annotations.value);
+
+watch(() => $q.lang.isoName, async() => {
+  if (chart.value) {
+    await chart.value.updateOptions(chartOptions.value);
+    chart.value.refresh();
+  }
+});
+</script>
+
+<template>
+  <apexchart
+    ref="chart"
+    width="100%"
+    height="200"
+    type="area"
+    :options="chartOptions"
+    :series="[{ data: history, name: props.location.address }]"
+  />
+</template>
+
+<style class="scss" scoped>
+</style>
